6eb010cf00295bf26c8b525d846b244732322c16
sarthu210 <sarthu102@gmail.com>
2026-07-25 23:50:49 +0530

    Inmemory User Is Created & Password Encode Added

--- changing-default-security-configuration/src/main/java/com/springbootsecurity/changingdefaultsecurityconfiguration/config/ProjectSecurtiyConfig.java
+++ changing-default-security-configuration/src/main/java/com/springbootsecurity/changingdefaultsecurityconfiguration/config/ProjectSecurtiyConfig.java
@@ -5,6 +5,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,6 +25,19 @@ public class ProjectSecurtiyConfig {
 		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("user").password("{bcrypt}$2a$12$qi1dJkvYFfhjF1zppykQLODxOCbz4GcENpp3nJgl4UEpFrg52.dBq").authorities("user").build();
+		UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$qi1dJkvYFfhjF1zppykQLODxOCbz4GcENpp3nJgl4UEpFrg52.dBq").authorities("admin").build();
+		
+		return new InMemoryUserDetailsManager(user,admin);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 }
