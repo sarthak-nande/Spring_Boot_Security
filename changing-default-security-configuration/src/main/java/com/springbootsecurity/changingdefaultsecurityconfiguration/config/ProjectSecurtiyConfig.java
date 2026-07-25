6a37c500c9e97ae51b81ec31fede2a832dc9d21a
sarthu210 <sarthu102@gmail.com>
2026-07-25 23:59:25 +0530

    Password Compramised Checker Added

--- changing-default-security-configuration/src/main/java/com/springbootsecurity/changingdefaultsecurityconfiguration/config/ProjectSecurtiyConfig.java
+++ changing-default-security-configuration/src/main/java/com/springbootsecurity/changingdefaultsecurityconfiguration/config/ProjectSecurtiyConfig.java
@@ -4,14 +4,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurtiyConfig {
@@ -39,5 +42,8 @@ public class ProjectSecurtiyConfig {
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
-
+	
+	public CompromisedPasswordChecker compromisedPasswordChecker() {
+		return new HaveIBeenPwnedRestApiPasswordChecker();
+	}
 }
